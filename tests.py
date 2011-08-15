354ae60249509b53962b2be72d084b7b74dbf3e0
mvr <matthewvonrocketstein@gmail-dot-com>
2011-08-15 07:38:30 -0400

    [master] outline for the other test cases

--- tests.py
+++ tests.py
@@ -4,10 +4,13 @@ from unittest2 import TestCase
 
 from channel import Channel, CallbackError, UnboundChannel
 
-class Tests(TestCase):
+class SimpleTests(TestCase):
+    """ These tests are more or less complete for simple dictionaries. """
     def setUp(self):
         self.exchange = {}
         self.main = Channel.MAIN
+        def callback(*args, **kargs): self.called_with=[args, kargs]
+        self.callback = callback
 
     def tearDown(self):
         self.main.bind(self.exchange)
@@ -15,30 +18,53 @@ class Tests(TestCase):
 
     def test_channels_cached(self):
         self.assertEqual(self.main, Channel.MAIN,
-                         "....")
+                         "should not have created a new channel "+\
+                         "when one of the same name already exists")
     def test_bind(self):
         self.main.bind(self.exchange)
         self.assertEqual(self.main._exchange,
                          self.exchange,
-                         '....')
+                         'expected bind would set the exchange for channel')
+
 
     def test_subscribers(self):
         with self.assertRaises(UnboundChannel):
             self.main.subscribers()
         self.main.bind(self.exchange)
-        self.assertEqual(self.main.subscribers(),[])
+        self.assertEqual(self.main.subscribers(),[],
+                         'virgin channel should have no susbcribers')
 
     def test_subscribe(self):
         self.main.bind(self.exchange)
         def f(**kargs): pass
         with self.assertRaises(CallbackError):
             self.main.subscribe(f)
-        def g(*args, **kargs): self.called_with=[args, kargs]
-        self.main.subscribe(g)
-        self.assertEqual([g],
-                         self.main.subscribers())
+        self.main.subscribe(self.callback)
+        self.assertEqual([self.callback],
+                         self.main.subscribers(),
+                         'subscription failed to propogate')
         self.main('testing')
         self.assertEqual([(), {'args': ('testing',)}],
+                         self.called_with,
+                         'subscriber was not notified')
+        del self.called_with
+        self.main.unsubscribe(self.callback)
+        self.assertEqual(self.main.subscribers(),[],
+                         'unsubscription did not take effect')
+
+    def test_subchannels(self):
+        self.main.bind(self.exchange)
+        subchan = self.main.sub
+        self.assertEqual(self.main.subchannels(),
+                         [subchan],
+                         'subchan did not show in subchannels() enumeration')
+        subchan.subscribe(self.callback)
+        subchan('testing')
+        self.assertEqual([(), {'args': ('testing',)}],
                          self.called_with)
+class ComplexTests(TestCase):
+    """ TODO: tests for more complex exchanges
+              that support subscribe(), etc
+    """
 if __name__=='__main__':
     import unittest2; unittest2.main()
