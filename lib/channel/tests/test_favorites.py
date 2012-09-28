3579128a0425d7bfc898da755198da932bbbdd46
mvr <matthewvonrocketstein@gmail-dot-com>
2012-09-27 23:02:00 -0400

    refactor

--- lib/channel/tests/test_favorites.py
+++ lib/channel/tests/test_favorites.py
@@ -1,26 +1,37 @@
 """ channel.tests.test_favorites
 """
 import copy
+
 from unittest2 import TestCase
 
 from channel import Channel
 
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return '<{0}>'.format(self.name.title())
+
+    @property
+    def vox(self):
+        return getattr(self.channel, self.name)
+
+    def listen_to(self, other):
+        other.vox.subscribe(self.ear)
+
+    def ear(self, *args, **kargs):
+        print self.name, 'receiving', args, kargs
 
 class TestFavorites(TestCase):
-    def test_foo(self):
+    def setUp(self):
         exchange = {}
         channel  = Channel.main
         channel.bind(exchange)
-        class Person:
-            def listen_to(self,other): other.vox.subscribe(self.ear)
+        self.alice = Person('self.alice')
+        self.bob   = Person('self.bob')
+        self.alice.vox = getattr(channel, 'self.alice')
+        self.bob.vox   = getattr(channel, 'self.bob')
 
-        def ear(*args, **kargs): print args, kargs
-        alice = Person(); alice.name='alice'
-        bob   = Person(); bob.name = 'bob'
-        alice.vox = getattr(channel, 'alice')
-        bob.vox   = getattr(channel, 'bob')
-        bob.ear = copy.copy(ear)
-        bob.listen_to(alice)
-
-if __name__=='__main__':
-    import unittest2; unittest2.main()
+    def test_1(self):
+        self.bob.listen_to(self.alice)
+        self.assertTrue(self.bob.ear in self.alice.vox.subscribers())
